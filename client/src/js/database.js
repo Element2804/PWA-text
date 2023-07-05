44bd59af8f4c0fdb47a9448acab0af6c329bae96
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-07-05 15:58:14 -0400

    get and post routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT in the Database');
+  const jateDB = await openDB('text', 1);
+  const tx = jateDB.transaction('text', 'readwrite');
+  const store = tx.objectStore('text');
+  const request = store.put({text: content});
+  const result = await request;
+  console.log('data saved', result); 
+ 
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+console.log("GET data from jateDB");
+const jateDB = await openDB('jateDB', 1);
+const tx = jateDB.transaction('jateDB', 'readonly');
+const store = tx.objectStore('jateDB');
+const request = store.getAll();
+const result = await request;
+
+console.log('result.value', result);
+
+};
 
 initdb();
